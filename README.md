e7464de1386accb977280f1ed39b043257241942
João Victor <joaocarvoli@hotmail.com>
2023-02-27 21:19:21 -0300

    Implementing more methods of the Linked List

--- README.md
+++ README.md
@@ -2,15 +2,22 @@
 
 ## Contents
 
+### Implementation Status
+
+- Not implemented: :no_entry:
+- Implementing: :warning:
+- Implemented: :white_check_mark:
+
 ### Lists
 - [Singly Linked](./lists/singlyLinked.py)
 
-| **Implemented** |  **Operation**   | **Method** | 🕛 **Time Complexity - (Worst)** |
-|:---------------:|:----------------:|:----------:|:--------------------------------:|
-|       [x]       |  Linear Search   |   search   |               O(N)               |
-|       [x]       | Add to the begin | add_start  |               O(1)               |
-|       [x]       |  Add to the end  |  add_end   |               O(1)               |
-|       [ ]       |      Remove      |    ...     |               O(N)               |
-|       [x]       |       Get        |    get     |               O(N)               |
-|       [x]       |      Update      |    set     |               O(N)               |
+|     **Status**     |      **Operation**      |      **Method**       | 🕛 **Time Complexity - (Worst)** |
+|:------------------:|:-----------------------:|:---------------------:|:--------------------------------:|
+| :white_check_mark: |      Linear Search      |        search         |               O(N)               |
+| :white_check_mark: |    Add to the begin     |       add_start       |               O(1)               |
+| :white_check_mark: | Add to the nth position |        add_at         |               O(N)               |
+| :white_check_mark: |     Add to the end      |        add_end        |               O(1)               |
+|     :warning:      |         Remove          | removeFirstOccurrence |               O(N)               |
+| :white_check_mark: |           Get           |          get          |               O(N)               |
+| :white_check_mark: |         Update          |          set          |               O(N)               |
 
--- lists/singlyLinked.py
+++ lists/singlyLinked.py
@@ -12,11 +12,45 @@ class SinglyLinkedList:
             node = SimpleNode(data)
             self._head = node
             self._tail = node
-            self._size += 1
         else:
             node = SimpleNode(data)
             node.next = self._head
             self._head = node
+            self._tail = node
+        self._size += 1
+
+    def add_at(self, data: int, position: int):
+        if self._head is None:
+            node = SimpleNode(data)
+            self._head = node
+            self._tail = node
+            self._size += 1
+        else:
+            counter = 0
+            previous_node = self._head
+            actual_node = self._head
+            node = SimpleNode(data)
+
+            if actual_node.next:
+                while actual_node.next and counter <= position:
+                    if counter > 0:
+                        previous_node = actual_node
+
+                    actual_node = actual_node.next
+                    counter += 1
+
+                previous_node.next = node
+                node.next = actual_node
+                self._tail = actual_node
+            else:
+                if position == 0:
+                    self._head = node
+                    node.next = previous_node
+                    self._tail = previous_node
+                else:
+                    previous_node.next = node
+                    self._tail = node
+        self._size += 1
 
     def add_end(self, data: int):
         if self._head is None:
@@ -34,7 +68,33 @@ class SinglyLinkedList:
                 self._tail = node
             self._size += 1
 
-    def get(self, index):
+    def remove_first_occurrence(self, data):
+        if self._head is None:
+            return "This element doesn't exists"
+        else:
+            element_found = False
+            counter = 0
+            previous_node = self._head
+            actual_node = self._head
+            while actual_node.next:
+                if counter > 0:
+                    previous_node = actual_node
+                if actual_node.data == data:
+                    element_found = True
+                    actual_node = actual_node.next
+                    break
+
+            if element_found:
+                if actual_node:
+                    previous_node.next = actual_node.next
+                elif actual_node.next is None:
+                    previous_node = None
+                del actual_node
+                return "Element removed"
+            else:
+                return "Element not found"
+
+    def get(self, index) -> int:
         temp = self._head
         for i in range(index):
             if temp:
@@ -58,7 +118,7 @@ class SinglyLinkedList:
         else:
             raise IndexError("list index out of range")
 
-    def search(self, elem):
+    def search(self, elem) -> str:
         if self._head.data is elem:
             return f'The element {elem} was found!'
         else:
@@ -74,13 +134,13 @@ class SinglyLinkedList:
             else:
                 return f"The element {elem} wasn't found!"
 
-    def size(self):
+    def size(self) -> int:
         return self._size
 
-    def get_head_data(self):
+    def get_head_data(self) -> int:
         return self._head.data
 
-    def get_tail_data(self):
+    def get_tail_data(self) -> int:
         return self._tail.data
 
     def print(self):
